b24b3b939c415cbf878852f179e2232bdf1b7903
hlfspune-bit <hlfspune@gmail.com>
2025-08-14 15:21:02 +0530

    Component  creation

---
+++ project/src/app/development/development.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-development',
+  imports: [],
+  templateUrl: './development.component.html',
+  styleUrl: './development.component.css'
+})
+export class DevelopmentComponent {
+
+}
---
+++ project/src/app/servicess/servicess.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicess',
+  imports: [],
+  templateUrl: './servicess.component.html',
+  styleUrl: './servicess.component.css'
+})
+export class ServicessComponent {
+
+}
